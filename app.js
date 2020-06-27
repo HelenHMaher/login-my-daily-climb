4c66a922d5245124d0b8b89834082cedb68fb678
HelenHMaher <helenharlanmaher@gmail.com>
2020-06-27 14:02:20 +0200

    server needs work

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const session = require("express-session");
 const bodyParser = require("body-parser");
-const mongo = require('mongodb').MongoClient;
+const mongo = require("mongodb").MongoClient;
 const routes = require("./routes.js");
 const auth = require("./auth.js");
 const app = express();
@@ -14,14 +14,18 @@ app.use(
 app.use(bodyParser.urlencoded({ extended: false }));
 
 mongo.connect(process.env.MONGO_URI, (err, client) => {
-    let db = client.db('myproject');
-    if(err) {
-      console.log('Database err: ' + err);
-    } else {
-      console.log('Successful database connection');
+  let db = client.db("myproject");
+  if (err) {
+    console.log("Database err: " + err);
+  } else {
+    console.log("Successful database connection");
 
-auth(app, db);
-routes(app, db);
+    auth(app, db);
+    routes(app, db);
 
-// start the server listening for requests
-app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
+    // start the server listening for requests
+    app.listen(process.env.PORT || 3000, () =>
+      console.log("Server is running...")
+    );
+  }
+});
