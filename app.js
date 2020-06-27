216a3068277eb0d942ae9e85ddcb06efb68407e6
HelenHMaher <helenharlanmaher@gmail.com>
2020-06-27 13:56:25 +0200

    mongo db

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const session = require("express-session");
 const bodyParser = require("body-parser");
+const mongo = require('mongodb').MongoClient;
 const routes = require("./routes.js");
 const auth = require("./auth.js");
 const app = express();
@@ -12,8 +13,15 @@ app.use(
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-auth(app);
-routes(app);
+mongo.connect(process.env.MONGO_URI, (err, client) => {
+    let db = client.db('myproject');
+    if(err) {
+      console.log('Database err: ' + err);
+    } else {
+      console.log('Successful database connection');
+
+auth(app, db);
+routes(app, db);
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
--- routes.js
+++ routes.js
@@ -1,7 +1,8 @@
 const passport = require("passport");
 const express = require("express");
+const { Db } = require("mongodb");
 
-module.exports = (app) => {
+module.exports = (app, db) => {
   function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
       next();
@@ -38,6 +39,40 @@ module.exports = (app) => {
     console.log("logout");
     res.redirect("/");
   });
+
+  app.route("/register").post(
+    (req, res, next) => {
+      Db.collection("my-daily-climb").findOne(
+        { username: req.body.username },
+        (err, user) => {
+          if (err) {
+            next(err);
+          } else if (user) {
+            res.redirect("/");
+          } else {
+            const hash = bcrypt.hashSync(req.body.password, 12);
+            Db.collection("my-daily-climb").insertOne(
+              {
+                username: req.body.username,
+                password: hash,
+              },
+              (err, doc) => {
+                if (err) {
+                  res.redirect("/");
+                } else {
+                  next(null, user);
+                }
+              }
+            );
+          }
+        }
+      );
+    },
+    passport.authenticate("local", { failureRedicret: "/" }),
+    (req, res, next) => {
+      res.redirect("https://my-daily-climb.herokuapp.com/");
+    }
+  );
 
   app.use((req, res, next) => {
     res.status(404).type("text").send("Not Found");
